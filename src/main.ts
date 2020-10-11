1287508dac0f74c5136d92d93b297153e2667de7
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-11 16:39:15 +0200

    Add start-db script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import config from './assets/config';
+import config from './config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
---
+++ start-db.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+SERVER="cms-database";
+PW="p2FXF1nTqvpe5plj";
+DB="main";
+
+echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
+(docker kill $SERVER || :) && \
+  (docker rm $SERVER || :) && \
+  docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
+  -e PGPASSWORD=$PW \
+  -p 5432:5432 \
+  -d postgres
+
+echo "sleep wait for pg-server [$SERVER] to start";
+SLEEP 3;
+
+echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
+echo "\l" | docker exec -i $SERVER psql -U postgres
