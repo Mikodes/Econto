c8e71d5e7e566a9ea985ee3be47943f157917ef9
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-12 07:03:02 +0200

    Update start-db.sh

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import configService from './config/config.service';
+import './config/database/generate-orm-config';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
 
 SERVER="cms-database";
 PW="p2FXF1nTqvpe5plj";
 DB="main";
 
-echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of [$SERVER]"
+echo "Removing old container [$SERVER] and starting new fresh instance of [$SERVER]"
 (docker kill $SERVER || :) && \
   (docker rm $SERVER || :) && \
   docker run --name $SERVER -e POSTGRES_PASSWORD=$PW \
@@ -13,8 +13,8 @@ echo "echo stop & remove old docker [$SERVER] and starting new fresh instance of
   -p 5432:5432 \
   -d postgres
 
-echo "sleep wait for pg-server [$SERVER] to start";
-SLEEP 3;
+echo "Waiting for [$SERVER] to start";
+SLEEP 5;
 
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
 echo "\l" | docker exec -i $SERVER psql -U postgres
