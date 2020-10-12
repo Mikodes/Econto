69c3c0c85d51bd53fe7acc4686a33e2cae2f654d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-12 14:27:41 +0200

    Create config.service.ts in postgres config

--- src/config/app/config.service.ts
+++ src/config/app/config.service.ts
@@ -4,6 +4,7 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class AppConfigService {
     constructor(private readonly _configService: ConfigService) {}
+    
     get port(): number {
         return Number(this._configService.get<number>('app.port'));
     }
