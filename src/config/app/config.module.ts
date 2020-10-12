6f51ad86468f9214942f1e6577588565310c528d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-12 14:24:43 +0200

    Create configuration.ts in postgres config

--- src/config/app/config.module.ts
+++ src/config/app/config.module.ts
@@ -1,18 +1,18 @@
-import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
 import configuration from './configuration';
 import { AppConfigService } from './config.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import validationSchema from './validation-schema';
+
+//TODO: Create more configs from .env
+//TODO: Remove unnecessary .env variables
+//TODO: Remove config.service.ts
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             load: [configuration],
-            validationSchema: Joi.object({
-                APP_PORT: Joi.number().default('4000'),
-                APP_MODE: Joi.string().valid('development', 'production', 'test').default('development'),
-                APP_PREFIX: Joi.string().default('/api/v1')
-            })
+            validationSchema: validationSchema
         })
     ],
     providers: [ConfigService, AppConfigService],
