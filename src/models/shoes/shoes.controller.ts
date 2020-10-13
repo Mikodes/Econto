c5d11553387d812035f2edf30fd2e73778b0677a
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-13 09:07:24 +0200

    Update shoes.controller.ts

---
+++ src/models/shoes/shoes.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateShoesDto } from './dtos/create.dto';
+import { Shoes } from './entities/shoes.entity';
+import { ShoesService } from './shoes.service';
+
+@Controller('shoes')
+export class ItemController {
+    constructor(private readonly _shoesService: ShoesService) { }
+
+    @Get()
+    public async getAll(): Promise<Shoes[]> {
+        return await this._shoesService.getAll();
+    }
+
+    @Post()
+    public async create(@Body() createShoesDto: CreateShoesDto): Promise<Shoes> {
+        return this._shoesService.create(createShoesDto.toEntity())
+    }
+}
