c23b30eae3ee0fb145e6c75a4181a174d55865bf
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-13 07:10:07 +0200

    Create toEntity method in create.dto.ts

--- src/models/shoes/dtos/create.dto.ts
+++ src/models/shoes/dtos/create.dto.ts
@@ -1,22 +1,42 @@
-import { IsEnum, IsNumber, IsString } from "class-validator";
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from "class-validator";
 import { Color, Gender } from "src/models/constants";
+import { Shoes } from "../entities/shoes.entity";
 
 export class CreateShoesDto implements Readonly<CreateShoesDto> {
     @IsString()
+    @IsNotEmpty()
     name: string;
 
     @IsNumber()
+    @IsNotEmpty()
     price: number;
 
     @IsString()
+    @IsNotEmpty()
     brand: string;
 
     @IsNumber()
+    @IsNotEmpty()
     size: number;
 
     @IsEnum(Color)
+    @IsNotEmpty()
     color: Color;
 
     @IsEnum(Gender)
+    @IsNotEmpty()
     gender: Gender;
+
+    public toEntity(): Shoes {
+        const entity = new Shoes();
+
+        entity.name = this.name;
+        entity.price = this.price;
+        entity.brand = this.brand;
+        entity.size = this.size;
+        entity.color = this.color;
+        entity.gender = this.gender;
+
+        return entity;
+    }
 }
