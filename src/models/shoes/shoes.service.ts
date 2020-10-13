bc61603dc1a84a5c61671099dd6853e049a9dbbb
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-13 09:46:45 +0200

    Update shoes.service.ts

--- src/models/shoes/shoes.service.ts
+++ src/models/shoes/shoes.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ShoesDto } from './dtos/shoes.dto';
 import { Shoes } from './entities/shoes.entity';
 
 @Injectable()
 export class ShoesService {
     constructor(@InjectRepository(Shoes) private readonly _shoesRepository: Repository<Shoes>) { }
 
-    public async getAll(): Promise<Shoes[]> {
-        return await this._shoesRepository.find();
+    public async getAll(): Promise<ShoesDto[]> {
+        return await this._shoesRepository.find().then(shoes => shoes.map(entity => ShoesDto.fromEntity(entity)));
     }
 
-    public async create(shoes: Shoes): Promise<Shoes> {
-        return this._shoesRepository.save(shoes);
+    public async create(shoesDto: ShoesDto): Promise<ShoesDto> {
+        return this._shoesRepository.save(shoesDto.toEntity()).then(entity => ShoesDto.fromEntity(entity));
     }
 }
