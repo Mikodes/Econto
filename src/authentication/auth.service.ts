83400cdc481c63c66a077510749b6fbe61c32424
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-14 15:47:29 +0200

    Move assign-object.ts to common/helpers

--- src/authentication/auth.service.ts
+++ src/authentication/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { UserNotFoundException } from "src/common/exceptions/user-not-found.exception";
 import { User } from "src/models/user/entities/user.entity";
 import { UsersService } from "src/models/user/users.service";
 
@@ -6,9 +7,11 @@ import { UsersService } from "src/models/user/users.service";
 export class AuthService  {
     constructor(private readonly _usersService: UsersService) {}
 
-    public async validateUser(username: string, password: string): Promise<boolean> {
+    public async validateUser(username: string, password: string): Promise<void> {
         const user: User = await this._usersService.getByUsername(username);
         
-        
+        if(!user) throw new UserNotFoundException();
+        if(user.password != password) {}
+
     }
 }
--- src/common/exceptions/user-not-found.exception.ts
+++ src/common/exceptions/user-not-found.exception.ts
@@ -1,6 +1,6 @@
 import { BaseException } from "./base.exception";
 
-export class ShoesNotFoundException extends BaseException {
+export class UserNotFoundException extends BaseException {
     id = 102;
     statusCode = 404;
     message = 'User does not exist'
