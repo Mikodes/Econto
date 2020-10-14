d5c0d036981627365eb4def1fbf02c82bfa4dcc6
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-14 07:55:18 +0200

    Create seeder.ts

--- src/database/seeders/shoes/seeder.ts
+++ src/database/seeders/shoes/seeder.ts
@@ -1,21 +1,46 @@
 import { Shoes } from "src/models/shoes/entities/shoes.entity";
-import faker from 'faker';
+import { random } from 'faker';
 import { Color, Gender } from "src/common/constants";
-import { createConnection } from "typeorm";
+import { Connection, ConnectionOptions, createConnection } from "typeorm";
 import { getOrmConfig } from "src/database/utils/read-orm-config";
+import { ShoesService } from "src/models/shoes/shoes.service";
+import { ShoesResponse } from "src/models/shoes/dtos/shoes.dto";
+import yargs from 'yargs';
+import { red, green } from 'chalk';
+
+run()
+    .then(() => console.log(green('Seeding completed successfully')))
+    .catch(error => console.log(red(error.message)));
 
 async function run(): Promise<void> {
-    // const connection = await createConnection();
-    console.log(getOrmConfig());
+    const amount: number = getAmountParameter();
+
+    const connection: Connection = await createConnection(getOrmConfig() as ConnectionOptions);
+    const shoesService: ShoesService = new ShoesService(connection.getRepository(Shoes));
+
+    for(let i = 0; i < amount; i++) {
+        const dto = ShoesResponse.fromObject(generateFakeShoes());
+        await shoesService.create(dto.toEntity());
+    }
+}
+
+function getAmountParameter(): number {
+    const args = yargs(process.env as any).argv;
+    const amount: number | undefined = args.amount as number | undefined;
+
+    if(amount === undefined) throw new Error('You need to specify the amount parameter by adding -- --amount=x to your script');
+    if(typeof amount !== 'number') throw new Error('Amount parameter must be a number');
+    
+    return amount;
 }
 
 function generateFakeShoes(): Partial<Shoes> {
     return {
-        name: faker.random.word(),
-        price: faker.random.number(1000),
-        brand: faker.random.word(),
-        size: faker.random.number(40),
-        color: faker.random.objectElement(Color) as Color,
-        gender: faker.random.objectElement(Gender) as Gender
+        name: random.word(),
+        price: random.number(1000),
+        brand: random.word(),
+        size: random.number(40),
+        color: random.objectElement(Color) as Color,
+        gender: random.objectElement(Gender) as Gender
     };
 }
--- src/database/utils/read-orm-config.ts
+++ src/database/utils/read-orm-config.ts
@@ -1,6 +1,6 @@
 import fs = require('fs');
 
-export function getOrmConfig(): Record<string, unknown> {
+export function getOrmConfig(): Record<string, any> {
     const config = fs.readFileSync('ormconfig.json', { encoding: 'utf8', flag: 'r' });
     const parsedConfig = JSON.parse(config);
 
