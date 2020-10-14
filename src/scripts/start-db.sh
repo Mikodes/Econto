e042c2a9d978c9cbceee3ea759b125c8199c30c2
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-14 08:13:10 +0200

    Remove run-prod script

--- src/scripts/start-db.sh
+++ src/scripts/start-db.sh
@@ -26,7 +26,7 @@ SLEEP 5;
 echo "CREATE DATABASE $DB ENCODING 'UTF-8';" | docker exec -i $SERVER psql -U postgres
 echo "\l" | docker exec -i $SERVER psql -U postgres
 
-if [ "$RUN_MIGRATIONS" ]; then
+if [ "$RUN_MIGRATIONS" == "true" ]; then
   echo "Running migrations...";
   npm run typeorm:migration:run
 fi
