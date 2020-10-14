c4a9fb4191da6cd8115794e7c8715b0b9d7ec52b
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-14 16:20:41 +0200

    Create auth.controller.ts

---
+++ src/authentication/auth.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, UseGuards, Post, Req } from "@nestjs/common";
+import { Request } from 'express';
+import { AuthGuard } from "@nestjs/passport";
+
+@Controller('auth')
+export class AuthController {
+    
+    @UseGuards(AuthGuard('local'))
+    @Post('/login')
+    async login(@Req() req: Request) {
+        return req.user;
+    }
+}
