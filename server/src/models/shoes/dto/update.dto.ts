20858488bf89cb8fa2215f7fbd8136297d28bd33
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-15 19:42:05 +0200

    Remove class-validator decorators from models/shoes/dto/update.dto.ts

--- server/src/models/shoes/dto/update.dto.ts
+++ server/src/models/shoes/dto/update.dto.ts
@@ -1,22 +1,15 @@
-import { IsEnum, IsNumber, IsString } from "class-validator";
 import { Color, Gender } from "../../../common/constants";
 
 export class UpdateShoesRequest implements Readonly<UpdateShoesRequest> {
-    @IsString()
     name: string;
 
-    @IsNumber()
     price: number;
 
-    @IsString()
     brand: string;
 
-    @IsNumber()
     size: number;
 
-    @IsEnum(Color)
     color: Color;
 
-    @IsEnum(Gender)
     gender: Gender;
 }
