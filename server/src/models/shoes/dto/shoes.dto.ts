1dc8b297954184267b43b9f96d3068f828b33bde
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-15 19:40:52 +0200

    Remove class-validator decorators from models/shoes/dto/shoes.dto.ts

--- server/src/models/shoes/dto/shoes.dto.ts
+++ server/src/models/shoes/dto/shoes.dto.ts
@@ -1,28 +1,20 @@
-import { IsEnum, IsNumber, IsString, IsUUID } from "class-validator";
 import { assignObject } from "../../../common/helpers/assign-object";
 import { Color, Gender } from "../../../common/constants";
 import { Shoes } from "../entities/shoes.entity";
 
 export class ShoesResponse implements Readonly<ShoesResponse> {
-    @IsUUID()
     id: string;
 
-    @IsString()
     name: string;
 
-    @IsNumber()
     price: number;
 
-    @IsString()
     brand: string;
 
-    @IsNumber()
     size: number;
 
-    @IsEnum(Color)
     color: Color;
 
-    @IsEnum(Gender)
     gender: Gender;
     
     public static fromObject(object: Partial<ShoesResponse>): ShoesResponse {
