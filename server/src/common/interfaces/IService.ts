33335d42b33c2bbc1f4f3b2ad97fd43762c98e2e
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-15 20:39:47 +0200

    Create interface/IService.ts

---
+++ server/src/common/interfaces/IService.ts
@@ -0,0 +1,11 @@
+export interface IService<T> {
+    getById(id: string): Promise<T | null>;
+
+    getAll(): Promise<T[]>;
+
+    create(entity: T): Promise<T>;
+
+    deleteById(id: string): Promise<void>;
+
+    updateById(id: string, shoes: Partial<T>): Promise<T>;
+}
