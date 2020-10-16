fc6240103de7bed1da02986e16993d2350bb5070
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-16 09:42:06 +0200

    Create config-validator.ts

--- server/src/common/exceptions/config-not-valid.exception.ts
+++
@@ -1,7 +0,0 @@
-export class ConfigNotValidException extends Error {
-    public constructor(variableName: string) {
-        super()
-
-        this.message = `Environment variable "${variableName}" does not exist or is invalid`;
-    }
-}
--- server/src/common/utils/config-validator.ts
+++ server/src/common/utils/config-validator.ts
@@ -1,13 +1,39 @@
 import { object, string, number } from '@hapi/joi';
+import { red } from 'chalk';
+import config from '../../config';
 
 export class ConfigValidator {
-    public validate(): void {
-
+    public validate(): boolean {
+        try {
+            const validationSchema = this.getValidationSchema();
+            validationSchema.validate(config);
+        } catch(error) {
+            this.printErrorMessage(error);
+            return false;
+        }
     }
 
     private getValidationSchema() {
         return object({
-            
+            APP: {
+                MODE: string().valid('development', 'production', 'test'),
+                PREFIX: string(),
+                PORT: number().min(80).max(10000)
+            },
+            AUTH: {
+                SECRET: string().min(10)
+            },
+            DATABASE: {
+                DATABASE_NAME: string().min(1),
+                DATABASE_HOST: string().ip(),
+                PASSWORD: string().min(1),
+                PORT: number().min(80).max(10000),
+                USER: string().min(1)
+            }
         });
+    }
+
+    private printErrorMessage(message: string): void {
+        console.log(red(message));
     }
 }
