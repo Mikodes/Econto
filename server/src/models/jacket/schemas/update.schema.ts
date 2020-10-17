329cecbe06e9d98f246f4851f82d67920935f558
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-17 06:46:27 +0200

    Create models/jacket/schemas/update.schema.ts

--- server/src/models/jacket/schemas/update.schema.ts
+++ server/src/models/jacket/schemas/update.schema.ts
@@ -1,4 +1,4 @@
 import { object } from '@hapi/joi';
 import { BaseJacketSchema } from './base.schema';
 
-export const CreateJacketSchema = object({ ...BaseJacketSchema });
+export const UpdateJacketSchema = object({ ...BaseJacketSchema });
