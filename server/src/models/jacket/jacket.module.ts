59dbfdb52c5bd666404f0a356cc2940ac9d85291
Piotr Blachnio <piotr.blachnio@yahoo.com>
2020-10-17 06:49:35 +0200

    Create jacket.module.ts

---
+++ server/src/models/jacket/jacket.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../../authentication/auth.module';
+import { JwtStrategy } from '../../authentication/strategies/jwt.strategy';
+import { Jacket } from './entities/jacket.entity';
+import { JacketsService } from './jackets.service';
+
+@Module({
+    imports: [AuthModule, TypeOrmModule.forFeature([Jacket])],
+    providers: [JacketsService, JwtStrategy],
+    controllers: [],
+    exports: []
+})
+
+export class JacketModule {}
